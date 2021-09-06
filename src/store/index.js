f19b234e8f283b7946e8ecea8e957673b9c2e8e2
Alex Paredes <alexparedesr95@gmail.com>
2021-09-05 22:12:30 -0500

    FEAT: Se crea mutación increment.

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,12 @@ export default createStore({
     state: {
         count: 1,
         lastMutation: 'none'
+    },
+    mutations: {
+        increment( state ) {
+            state.count++
+            state.lastMutation = 'increment'
+        }
     }
 
 })
