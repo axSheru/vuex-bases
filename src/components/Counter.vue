7ce6f10c51a3340f722813dda367189b5580f531
Alex Paredes <alexparedesr95@gmail.com>
2021-09-05 21:58:37 -0500

    FEAT: Se leen atributos del state en el Counter component.

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,9 +1,29 @@
 <template>
   <h1>Counter - Vuex</h1>
+  <h2>Direct access: {{ $store.state.count }}</h2>
+  <h2>Computed: {{ countComputed }}</h2>
+
+  <h1>mapState</h1>
+  <h2>mapState: {{ count }}</h2>
+  <h2>lastMutation: {{ lastMutation }}</h2>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
+
+    computed: {
+        countComputed() {
+            return this.$store.state.count
+        },
+        ...mapState(['count', 'lastMutation'])//Opción 1 con mapState
+        /* ...mapState({
+            count: state => state.count,
+            lastMutation: state => state.lastMutation
+        })//Opción 2 con mapState */
+    }
 
 }
 </script>
