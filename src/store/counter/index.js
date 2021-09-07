f370231b51a86b38797f88621f47359bb7b6d42b
Alex Paredes <alexparedesr95@gmail.com>
2021-09-07 00:36:35 -0500

    REFACTOR: Se separan los getters del counterStore a su propio archivo.

--- src/store/counter/index.js
+++ src/store/counter/index.js
@@ -1,6 +1,7 @@
 import state from './state'
 import * as mutations from './mutations'
 import * as actions from './actions'
+import * as getters from './getters'
 
 const counterStore = {
 
@@ -10,11 +11,7 @@ const counterStore = {
     state,
     mutations,
     actions,
-    getters: {
-        squareCount( state ) {
-            return state.count * state.count
-        }
-    }
+    getters,
 
 }
 
